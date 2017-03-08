400a4b4deed2f05cb224b77c0d7372ef650ad959
Ywj <somnus.yuwenjia@foxmail.com>
2017-03-08 20:39:20 +0800

    add 189.js

---
+++ 189.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    var len = nums.length;
+    for (var i =1 ; i <= k; i ++ ) {
+        nums.unshift(nums.pop());
+    }
+};
