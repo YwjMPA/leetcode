d4f014815d428a9085f7764ea9d58dfec30bcd72
Ywj <somnus.yuwenjia@foxmail.com>
2017-03-27 11:42:32 +0800

    add 367.js

---
+++ 367.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    for (var i = 1; i <= num; i ++){
+        if (i * i == num){
+            return true;
+        }else if(i * i > num){
+            return false;
+        }
+    }
+};
